5ad2b16e0e70278ab0b134803a49c98d9676a491
hoyaXie <32215945+hoyaXie@users.noreply.github.com>
2017-11-08 18:15:12 +0800

    Initial commit

---
+++ js/LivellPayroll.js
@@ -0,0 +1,30 @@
+var ServerAddress = 'http://192.168.1.3:57110/AppServer/';
+//var ServerAddress = 'http://app.livell.com/AppServer/';
+
+function ServeURL(action) {
+	return ServerAddress + action;
+};
+
+function scrollUp() {
+	if(mui.os.android) {
+		window.scrollTo(0, 0);
+	} else if(mui.os.ios) {
+		mui('.mui-scroll-wrapper').scroll().scrollTo(0, 0, 100); //100毫秒滚动到顶  
+	}
+};
+
+(function($, document) {
+	var btn_callcustom = document.getElementsByClassName("bar-btn-callcustom")[0];
+	if(btn_callcustom != null) {
+		btn_callcustom.addEventListener('tap', function() {
+			var btnArray = ['NO', 'YES'];
+			var Phone = "502-2590936";
+			$.confirm('Make sure you call livellpayroll customer service ？', 'Confirm', btnArray, function(e) {
+				if(e.index == 1) {
+					plus.device.dial(Phone, false);
+				}
+			});
+		});
+	};
+
+})(mui, document);
---
+++ js/app.js
@@ -0,0 +1,104 @@
+/**
+ * 演示程序当前的 “注册/登录” 等操作，是基于 “本地存储” 完成的
+ * 当您要参考这个演示程序进行相关 app 的开发时，
+ * 请注意将相关方法调整成 “基于服务端Service” 的实现。
+ **/
+(function($, owner) {
+	/**
+	 * 用户登录
+	 **/
+	owner.login = function(loginInfo, callback) {
+		callback = callback || $.noop;
+		loginInfo = loginInfo || {};
+		loginInfo.account = loginInfo.account || '';
+		loginInfo.password = loginInfo.password || '';
+		if(!checkEmail(loginInfo.account)) {
+			return callback('The email is not valid');
+		}
+		if(loginInfo.password.length < 6) {
+			return callback('The password is not valid');
+		}
+		var users = JSON.parse(localStorage.getItem('$users') || '[]');
+		var authed = users.some(function(user) {
+			return loginInfo.account == user.account && loginInfo.password == user.password;
+		});
+
+		mui.ajax({
+			url: ServeURL('AppLogin'), //跨域URL
+			data: loginInfo,
+			type: "GET",
+			dataType: "json",
+			timeout: 5000, //超时时间设置为5秒；
+			success: function(data) {
+				if(data.status == 'success') {
+					plus.storage.setItem('userid', data.userId);
+					return owner.createState(loginInfo.account, callback);
+				} else {
+					return callback(data.message);
+				}
+			},
+			error: function(xhr, type, errorThrown) {
+				return callback(type);
+			}
+		});
+	};
+
+	owner.createState = function(name, callback) {
+		var state = owner.getState();
+		state.account = name;
+		state.token = "token123456789";
+		owner.setState(state);
+		return callback();
+	};
+
+	/**
+	 * 获取当前状态
+	 **/
+	owner.getState = function() {
+		var stateText = localStorage.getItem('$state') || "{}";
+		return JSON.parse(stateText);
+	};
+
+	/**
+	 * 设置当前状态
+	 **/
+	owner.setState = function(state) {
+		state = state || {};
+		localStorage.setItem('$state', JSON.stringify(state));
+		//var settings = owner.getSettings();
+		//settings.gestures = '';
+		//owner.setSettings(settings);
+	};
+
+	var checkEmail = function(email) {
+		email = email || '';
+		return(email.length > 3 && email.indexOf('@') > -1);
+	};
+
+	/**
+	 * 找回密码
+	 **/
+	owner.forgetPassword = function(email, callback) {
+		callback = callback || $.noop;
+		if(!checkEmail(email)) {
+			return callback('邮箱地址不合法');
+		}
+		return callback(null, '新的随机密码已经发送到您的邮箱，请查收邮件。');
+	};
+
+	/**
+	 * 获取应用本地配置
+	 **/
+	owner.setSettings = function(settings) {
+		settings = settings || {};
+		localStorage.setItem('$settings', JSON.stringify(settings));
+	}
+
+	/**
+	 * 设置应用本地配置
+	 **/
+	owner.getSettings = function() {
+		var settingsText = localStorage.getItem('$settings') || "{}";
+		return JSON.parse(settingsText);
+	}
+}(mui, window.app = {}));
